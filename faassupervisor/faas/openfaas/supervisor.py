14d071d6bb937a0ae48b390d1d54e0342c636255
Sebas Risco <sebas.risco@gmail.com>
2019-06-11 17:16:28 +0200

    Load script from base64 env var in Openfaas

--- faassupervisor/faas/openfaas/supervisor.py
+++ faassupervisor/faas/openfaas/supervisor.py
@@ -18,21 +18,28 @@ import subprocess
 import sys
 from faassupervisor.faas.default import DefaultSupervisor
 from faassupervisor.logger import get_logger
-from faassupervisor.utils import SysUtils
+from faassupervisor.utils import SysUtils, FileUtils, StrUtils
 
 
 class OpenfaasSupervisor(DefaultSupervisor):
     """Supervisor class used in the OpenFaaS environment."""
 
+    _SCRIPT_FILE_NAME = 'script.sh'
+
     def __init__(self):
         get_logger().info('SUPERVISOR: Initializing Openfaas supervisor')
 
     def execute_function(self):
-        script = SysUtils.get_env_var('sprocess')
-        if script:
-            get_logger().info("Executing user defined script: '%s'", script)
+        if SysUtils.is_var_in_env('SCRIPT'):
+            script_path = SysUtils.join_paths(SysUtils.get_env_var("TMP_INPUT_DIR"),
+                                              self._SCRIPT_FILE_NAME)
+            script_content = StrUtils.base64_to_str(SysUtils.get_env_var('SCRIPT'))
+            FileUtils.create_file_with_content(script_path, script_content)
+            get_logger().info("Script file created in '%s'", script_path)
+            FileUtils.set_file_execution_rights(script_path)
+            get_logger().info("Executing user defined script: '%s'", script_path)
             try:
-                script_output = subprocess.check_output(['/bin/sh', script],
+                script_output = subprocess.check_output(['/bin/sh', script_path],
                                                         stderr=subprocess.STDOUT).decode("latin-1")
                 get_logger().info(script_output)
             except subprocess.CalledProcessError as cpe:
