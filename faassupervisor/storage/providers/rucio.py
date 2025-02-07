0d696fe4c42f89da4740f8077010721ab364bb2b
Miguel Caballer <micafer1@upv.es>
2025-02-07 08:24:51 +0100

    Minor changes

--- faassupervisor/storage/providers/rucio.py
+++ faassupervisor/storage/providers/rucio.py
@@ -21,14 +21,14 @@ import tempfile
 from rucio.rse.protocols import posix, webdav, storm, ssh, rclone, xrootd # noqa pylint: disable=unused-import
 import dogpile.cache.backends.memory # noqa pylint: disable=unused-import
 
-from faassupervisor.logger import get_logger
-from faassupervisor.storage.providers import DefaultStorageProvider
-from faassupervisor.utils import SysUtils
 from rucio.client.client import Client
 from rucio.client.uploadclient import UploadClient
 from rucio.client.downloadclient import DownloadClient
 from rucio.common.exception import DataIdentifierAlreadyExists, NoFilesUploaded
 from faassupervisor.exceptions import RucioDataIdentifierAlreadyExists
+from faassupervisor.logger import get_logger
+from faassupervisor.storage.providers import DefaultStorageProvider
+from faassupervisor.utils import SysUtils
 
 
 class Rucio(DefaultStorageProvider):
